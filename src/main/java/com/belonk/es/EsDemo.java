557b1846e0b3b0027a560fec35d06193c437eaae
sunfuchang03@126.com <swuxiami03SUN>
2017-06-19 17:33:44 +0800

    commit

--- src/main/java/com/belonk/es/EsDemo.java
+++ src/main/java/com/belonk/es/EsDemo.java
@@ -2,21 +2,24 @@ package com.belonk.es;
 
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.DocWriteResponse;
-import org.elasticsearch.action.bulk.BulkRequestBuilder;
-import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.bulk.*;
 import org.elasticsearch.action.bulk.byscroll.BulkByScrollResponse;
 import org.elasticsearch.action.bulk.byscroll.BulkByScrollTask;
+import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.*;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.unit.ByteSizeUnit;
+import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentType;
@@ -27,6 +30,8 @@ import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.script.Script;
 import org.elasticsearch.script.ScriptService;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHitField;
+import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
@@ -41,6 +46,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import static org.elasticsearch.index.query.QueryBuilders.termQuery;
 
@@ -236,6 +242,8 @@ public class EsDemo {
         }
     }
 
+    // bulk test start =================================================================================================
+
     public void bulk() throws IOException {
         BulkRequestBuilder bulkRequest = client.prepareBulk();
         // either use client#prepare, or use Requests# to directly build index/delete requests
@@ -266,25 +274,258 @@ public class EsDemo {
         }
     }
 
-    public void bulkProccessor() {
+    // 异步执行
+    public void bulkProccessor() throws InterruptedException, IOException {
+        BulkProcessor bulkProcessor = BulkProcessor.builder(
+                client, // 客户端
+                new BulkProcessor.Listener() {
+                    /**
+                     * This method is called just before bulk is executed. You can for example see the numberOfActions
+                     * with request.numberOfActions().
+                     *
+                     * @param executionId
+                     * @param request
+                     */
+                    @Override
+                    public void beforeBulk(long executionId, BulkRequest request) {
+                        System.out.println("beforeBulk...");
+                        System.out.println("executionId : " + executionId);
+                        System.out.println("request.numberOfActions() : " + request.numberOfActions());
+                    }
 
+                    /**
+                     * This method is called after bulk execution. You can for example check if there was some
+                     * failing requests with response.hasFailures().
+                     *
+                     * @param executionId
+                     * @param request
+                     * @param response
+                     */
+                    @Override
+                    public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+                        System.out.println("after bulk is execution finished...");
+                        System.out.println("executionId : " + executionId);
+                        System.out.println("response.hasFailures() : " + response.hasFailures());
+                    }
+
+                    /**
+                     * This method is called when the bulk failed and raised a Throwable
+                     */
+                    @Override
+                    public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+                        System.out.println("after bulk execution is failed ...");
+                        System.out.println("executionId : " + executionId);
+                    }
+                })
+                /*
+                 * We want to execute the bulk every 10 000 requests, default is 1000.
+                 */
+                .setBulkActions(10000)
+                /*
+                 * We want to flush the bulk every 5mb, default value is 5m.
+                 */
+                .setBulkSize(new ByteSizeValue(5, ByteSizeUnit.MB))
+                /*
+                 * We want to flush the bulk every 5 seconds whatever the number of requests. BulkProcessor does not set
+                 * flushinterval by default.
+                 */
+                .setFlushInterval(TimeValue.timeValueSeconds(5))
+                /*
+                 * Set the number of concurrent requests. A value of 0 means that only a single request will be allowed
+                 * to be executed. A value of 1 means 1 concurrent request is allowed to be executed while accumulating
+                 * new bulk requests.
+                 *
+                 * sets concurrentRequests to 1, which means an asynchronous execution of the flush operation.
+                 */
+                .setConcurrentRequests(1)
+                /*
+                 * Set a custom backoff policy which will initially wait for 100ms, increase exponentially and retries
+                 * up to three times. A retry is attempted whenever one or more bulk item requests have failed with an
+                 * EsRejectedExecutionException which indicates that there were too little compute resources available
+                 * for processing the request. To disable backoff, pass BackoffPolicy.noBackoff().
+                 *
+                 * sets backoffPolicy to an exponential backoff with 8 retries and a start delay of 50ms. The total wait
+                 * time is roughly 5.1 seconds.
+                 */
+                .setBackoffPolicy(BackoffPolicy.exponentialBackoff(TimeValue.timeValueMillis(100), 3))
+                .build();
+
+        // add requests.
+        bulkProcessor.add(new IndexRequest("twitter", "tweet", "1").source(
+                XContentFactory.jsonBuilder().startObject()
+                        .field("name", "testtwitter")
+                        .field("date", new Date())
+                        .field("content", "haha, this is a test twitter.")
+                        .endObject()
+        ));
+        bulkProcessor.add(new DeleteRequest("twitter", "tweet", "2"));
+
+        /*
+         * close the bulk processor.
+         *
+         * Both methods flush any remaining documents and disable all other scheduled flushes if they were scheduled by
+         * setting flushInterval
+         */
+        /*
+         *  If concurrent requests were enabled the awaitClose method waits for up to the specified timeout for all bulk
+         *  requests to complete then returns true, if the specified waiting time elapses before all bulk requests
+         *  complete, false is returned.
+         */
+        bulkProcessor.awaitClose(10, TimeUnit.MINUTES);
+        /*
+         * The close method doesn’t wait for any remaining bulk requests to complete and exits immediately.
+         */
+        // bulkProcessor.close()
     }
 
-    // search api test
-    public void scroll() {
-        QueryBuilder qb = termQuery("multi", "test");
+    // 同步执行，测试用
+    public void bulkProcessorSync() throws IOException {
+        BulkProcessor bulkProcessor = BulkProcessor.builder(client, new BulkProcessor.Listener() {
+            @Override
+            public void beforeBulk(long executionId, BulkRequest request) {
+                System.out.println("beforeBulk...");
+                System.out.println("executionId : " + executionId);
+                System.out.println("request.numberOfActions() : " + request.numberOfActions());
+            }
 
-        SearchResponse scrollResp = client.prepareSearch("test")
+            @Override
+            public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+                System.out.println("after bulk is execution finished...");
+                System.out.println("executionId : " + executionId);
+                System.out.println("response.hasFailures() : " + response.hasFailures());
+            }
+
+            @Override
+            public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+                System.out.println("after bulk execution is failed ...");
+                System.out.println("executionId : " + executionId);
+                failure.printStackTrace();
+            }
+        })
+                .setBulkActions(10000)
+                .setConcurrentRequests(0)
+                .build();
+
+        // Add your requests
+        bulkProcessor.add(new IndexRequest("twitter", "tweet", "1").source(
+                XContentFactory.jsonBuilder().startObject()
+                        .field("name", "testtwitter")
+                        .field("date", new Date())
+                        .field("content", "haha, this is a test twitter.")
+                        .endObject()
+        ));
+        bulkProcessor.add(new DeleteRequest("twitter", "tweet", "22"));
+
+        // Flush any remaining requests
+        bulkProcessor.flush();
+
+        // Or close the bulkProcessor if you don't need it anymore
+        bulkProcessor.close();
+
+        // Refresh your indices
+        client.admin().indices().prepareRefresh().get();
+
+        // Now you can start searching!
+        client.prepareSearch().get();
+    }
+
+    // bulk test end ===================================================================================================
+
+    // search api test start ===========================================================================================
+    public void basicSearch() {
+        /*
+        $ CURL -XGET "localhost:9200/user/info/_search?q=username:sun1&pretty"
+        {
+          "took" : 7,
+          "timed_out" : false,
+          "_shards" : {
+            "total" : 5,
+            "successful" : 4,
+            "failed" : 0
+          },
+          "hits" : {
+            "total" : 1,
+            "max_score" : 0.2876821,
+            "hits" : [
+              {
+                "_index" : "user",
+                "_type" : "info",
+                "_id" : "1",
+                "_score" : 0.2876821,
+                "_source" : {
+                  "age" : 100,
+                  "gender" : 1,
+                  "hovers" : [
+                    "eat",
+                    "music",
+                    "programming"
+                  ],
+                  "id" : 1,
+                  "password" : "123456",
+                  "username" : "sun1"
+                }
+              }
+            ]
+          }
+        }
+        */
+        SearchResponse response = client.prepareSearch(indexName)
+                .setTypes(indexType) // empty is all types
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(QueryBuilders.termQuery("username", "sun1"))                 // Query
+                .setPostFilter(QueryBuilders.rangeQuery("age").gte(100))     // Filter
+                .setFrom(0).setSize(60).setExplain(true)
+                .get();
+        print(response);
+    }
+
+    public void basicSearch1() {
+        print(client.prepareSearch().get());
+    }
+
+    private void print(SearchResponse response) {
+        SearchHits searchHits = response.getHits();
+        System.out.println("total hits     : " + searchHits.getTotalHits());
+        System.out.println("hits           : " + searchHits.getHits().length);
+        System.out.println("getMaxScore()  : " + searchHits.getMaxScore());
+        System.out.println("internalHits() :" + searchHits.internalHits().length);
+        System.out.println("............................");
+        for (SearchHit searchHit : searchHits) {
+            System.out.println(searchHit);
+            System.out.println("docId      ： " + searchHit.docId());
+
+            Map<String, SearchHitField> map = searchHit.getFields();
+            System.out.println("fields     : " + map.size());
+            for (String s : map.keySet()) {
+                System.out.println(s + " : " + map.get(s));
+            }
+            System.out.println("getScore() : " + searchHit.getScore());
+            System.out.println("getShard() : " + searchHit.getShard());
+            System.out.println("getIndex() : " + searchHit.getIndex());
+            System.out.println("getType()  : " + searchHit.getType());
+
+            Map<String, Object> sourceMap = searchHit.getSource();
+            System.out.println("sources : " + sourceMap.size());
+            for (String s : sourceMap.keySet()) {
+                System.out.println(s + " : " + sourceMap.get(s));
+            }
+            System.out.println("getExplanation : " + searchHit.getExplanation());
+        }
+    }
+
+    public void scroll() {
+        QueryBuilder qb = termQuery("username", "sun1");
+
+        SearchResponse scrollResp = client.prepareSearch(indexName)
                 .addSort(FieldSortBuilder.DOC_FIELD_NAME, SortOrder.ASC)
-                .setScroll(new TimeValue(60000))
+                .setScroll(new TimeValue(6000))
                 .setQuery(qb)
                 .setSize(100).get(); //max of 100 hits will be returned for each scroll
         //Scroll until no hits are returned
         do {
             for (SearchHit hit : scrollResp.getHits().getHits()) {
-                //Handle the hit...
+                System.out.println("total hits     : " + hit.getSourceAsString());
             }
-
             scrollResp = client.prepareSearchScroll(scrollResp.getScrollId()).setScroll(new TimeValue(60000)).execute().actionGet();
         }
         while (scrollResp.getHits().getHits().length != 0); // Zero hits mark the end of the scroll and the while loop.
@@ -358,7 +599,15 @@ public class EsDemo {
 //        String[] ids = new String[]{"1", "3"};
 //        demo.multiGet(ids);
 
-        demo.bulk();
+//        demo.bulk();
+//        demo.bulkProccessor();
+//        demo.bulkProcessorSync();
+
+//        demo.basicSearch();
+//        demo.basicSearch1();
+
+        demo.scroll();
+
         demo.closeClient();
     }
 }
